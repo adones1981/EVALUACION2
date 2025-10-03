efe7098cd13a885b33b2b21058f591c8200a673d
Julio Adones <juliadones81@gmail.com>
2025-10-03 08:42:10 -0300

    Corrigiendo: agregando miportafolio como carpeta normal

---
+++ miportafolio/proyectos/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def inicio(request):
+    return render(request, "proyectos/inicio.html")
+
+def lista_proyectos(request):
+    proyectos = [
+        {"titulo": "Proyecto 1", "descripcion": "Descripción del proyecto 1"},
+        {"titulo": "Proyecto 2", "descripcion": "Descripción del proyecto 2"},
+        {"titulo": "Proyecto 3", "descripcion": "Descripción del proyecto 3"},
+    ]
+    return render(request, "proyectos/lista_proyectos.html", {"proyectos": proyectos})
