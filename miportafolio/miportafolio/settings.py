33373acd394fa394abdd2e2cbc041fdc03ebdcb2
Julio Adones <tu_correo@ejemplo.com>
2025-10-04 17:20:13 -0300

    Agregar ALLOWED_HOSTS para Render

--- miportafolio/miportafolio/settings.py
+++ miportafolio/miportafolio/settings.py
@@ -4,7 +4,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-secretkey123"
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ "evaluacion2-n8x4.onrender.com"]
+
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
