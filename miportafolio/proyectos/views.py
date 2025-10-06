1c0a261c601e6c63e76552dfe50259fe00de9a77
adones1981 <julioadones81@gmail.com>
2025-10-06 19:03:43 -0300

    Update views.py

--- miportafolio/proyectos/views.py
+++ miportafolio/proyectos/views.py
@@ -11,14 +11,3 @@ def lista_proyectos(request):
     ]
     return render(request, "proyectos/lista_proyectos.html", {"proyectos": proyectos})
 
-
-from django.shortcuts import render, get_object_or_404
-from .models import Question
-
-def question_list(request):
-    questions = Question.objects.order_by('-pub_date')
-    return render(request, 'miportafolio/question_list.html', {'questions': questions})
-
-def question_detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'miportafolio/question_detail.html', {'question': question})
